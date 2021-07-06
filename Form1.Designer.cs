1f1d56608c41d63c2990cf098a152e6671d4704e
beraatKoyuncu <beraat.koyuncu@gmail.com>
2021-07-06 15:53:50 +0300

    commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,240 @@
+﻿
+namespace TelephoneDirectory
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.textBoxSurname = new System.Windows.Forms.TextBox();
+            this.textBoxNumber = new System.Windows.Forms.MaskedTextBox();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.buttonDelete = new System.Windows.Forms.Button();
+            this.buttonUpdate = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.c_id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.c_name = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.c_surname = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.c_telephone = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.backgroundWorker2 = new System.ComponentModel.BackgroundWorker();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(25, 30);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(20, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Ad";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(25, 59);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Soyad";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(25, 88);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(43, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Telefon";
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Location = new System.Drawing.Point(107, 27);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(124, 20);
+            this.textBoxName.TabIndex = 5;
+            this.textBoxName.TextChanged += new System.EventHandler(this.textBoxName_TextChanged);
+            // 
+            // textBoxSurname
+            // 
+            this.textBoxSurname.Location = new System.Drawing.Point(107, 56);
+            this.textBoxSurname.Name = "textBoxSurname";
+            this.textBoxSurname.Size = new System.Drawing.Size(124, 20);
+            this.textBoxSurname.TabIndex = 7;
+            // 
+            // textBoxNumber
+            // 
+            this.textBoxNumber.Location = new System.Drawing.Point(107, 85);
+            this.textBoxNumber.Mask = "(999) 000 00 00";
+            this.textBoxNumber.Name = "textBoxNumber";
+            this.textBoxNumber.Size = new System.Drawing.Size(124, 20);
+            this.textBoxNumber.TabIndex = 8;
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Location = new System.Drawing.Point(392, 22);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(88, 28);
+            this.buttonAdd.TabIndex = 11;
+            this.buttonAdd.Text = "EKLE";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
+            // 
+            // buttonDelete
+            // 
+            this.buttonDelete.Location = new System.Drawing.Point(392, 51);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.Size = new System.Drawing.Size(88, 28);
+            this.buttonDelete.TabIndex = 12;
+            this.buttonDelete.Text = "SİL";
+            this.buttonDelete.UseVisualStyleBackColor = true;
+            this.buttonDelete.Click += new System.EventHandler(this.buttonDelete_Click);
+            // 
+            // buttonUpdate
+            // 
+            this.buttonUpdate.Location = new System.Drawing.Point(392, 80);
+            this.buttonUpdate.Name = "buttonUpdate";
+            this.buttonUpdate.Size = new System.Drawing.Size(88, 28);
+            this.buttonUpdate.TabIndex = 13;
+            this.buttonUpdate.Text = "GÜNCELLE";
+            this.buttonUpdate.UseVisualStyleBackColor = true;
+            this.buttonUpdate.Click += new System.EventHandler(this.buttonUpdate_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.c_id,
+            this.c_name,
+            this.c_surname,
+            this.c_telephone});
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(3, 16);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(517, 287);
+            this.dataGridView1.TabIndex = 14;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // c_id
+            // 
+            this.c_id.HeaderText = "Id";
+            this.c_id.Name = "c_id";
+            this.c_id.ReadOnly = true;
+            this.c_id.Visible = false;
+            // 
+            // c_name
+            // 
+            this.c_name.HeaderText = "Ad";
+            this.c_name.Name = "c_name";
+            this.c_name.ReadOnly = true;
+            // 
+            // c_surname
+            // 
+            this.c_surname.HeaderText = "Soyad";
+            this.c_surname.Name = "c_surname";
+            this.c_surname.ReadOnly = true;
+            // 
+            // c_telephone
+            // 
+            this.c_telephone.HeaderText = "Telefon";
+            this.c_telephone.Name = "c_telephone";
+            this.c_telephone.ReadOnly = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.dataGridView1);
+            this.groupBox1.Location = new System.Drawing.Point(-1, 142);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(523, 306);
+            this.groupBox1.TabIndex = 15;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Kayıtlı Numaralar";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // backgroundWorker2
+            // 
+            this.backgroundWorker2.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker2_DoWork);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(521, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.buttonUpdate);
+            this.Controls.Add(this.buttonDelete);
+            this.Controls.Add(this.buttonAdd);
+            this.Controls.Add(this.textBoxNumber);
+            this.Controls.Add(this.textBoxSurname);
+            this.Controls.Add(this.textBoxName);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.TextBox textBoxSurname;
+        private System.Windows.Forms.MaskedTextBox textBoxNumber;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.Button buttonDelete;
+        private System.Windows.Forms.Button buttonUpdate;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn c_id;
+        private System.Windows.Forms.DataGridViewTextBoxColumn c_name;
+        private System.Windows.Forms.DataGridViewTextBoxColumn c_surname;
+        private System.Windows.Forms.DataGridViewTextBoxColumn c_telephone;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,265 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TelephoneDirectory
+{
+    public partial class Form1 : Form
+    {
+
+        SqlConnection connection;
+        SqlCommand command;
+
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        void Listing()
+        {
+            connection = new SqlConnection("Data Source=DESKTOP-Q9BA0JM;Initial Catalog=TelephoneBook;Integrated Security=True");
+            command = new SqlCommand("select[id], [name], [surname], [telephone] from Persons", connection);
+            dataGridView1.Rows.Clear();
+            try
+            {
+                connection.Open();
+                SqlDataReader dataReader = command.ExecuteReader();
+                while (dataReader.Read())
+                {
+                    
+                    dataGridView1.Rows.Add(dataReader.GetInt32(0),dataReader.GetString(1),dataReader.GetString(2),dataReader.GetString(3) );
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Numaralar alınamadı.");
+                
+            }
+            finally
+            {
+                if(connection.State != ConnectionState.Closed)
+                {
+                    connection.Close();
+                }
+            }
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Listing();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            int result = -1;
+            connection = new SqlConnection("Data Source=DESKTOP-Q9BA0JM;Initial Catalog=TelephoneBook;Integrated Security=True");
+            command = new SqlCommand("insert into Persons([name], [surname], [telephone]) values(@n, @s, @t)", connection);
+            command.Parameters.AddWithValue("@n", textBoxName.Text);
+            command.Parameters.AddWithValue("@s", textBoxSurname.Text);
+            command.Parameters.AddWithValue("@t", textBoxNumber.Text);
+
+            try
+            {
+                connection.Open();
+                result = command.ExecuteNonQuery();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Bağlantıda hata oluştu.");
+
+            }
+            finally
+            {
+                if (connection.State != ConnectionState.Closed)
+                {
+                    connection.Close();
+                }
+            }
+            if (result > 0)
+            {
+                MessageBox.Show("Telefon numarası kaydedildi.");
+                Listing();
+            }
+            else
+            {
+                MessageBox.Show("Telefon numarası kaydedilemedi.");
+                
+            }
+
+           
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+            if (dataGridView1.SelectedRows.Count == 0)
+            {
+                return;
+            }
+
+            object value = dataGridView1.SelectedRows[0].Cells[0].Value;
+            int id = (int)value;
+            connection = new SqlConnection("Data Source=DESKTOP-Q9BA0JM;Initial Catalog=TelephoneBook;Integrated Security=True");
+            command = new SqlCommand("select top 1 [id], [name], [surname], [telephone] from Persons where id=@id", connection);
+            command.Parameters.AddWithValue("@id", id);
+            try
+            {
+                connection.Open();
+                SqlDataReader dataReader = command.ExecuteReader();
+                while (dataReader.Read())
+                {
+                    textBoxName.Text = dataReader.GetString(1);
+                    textBoxSurname.Text = dataReader.GetString(2);
+                    textBoxNumber.Text = dataReader.GetString(3);
+                }
+
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Telefon numarası alınamadı.", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            }
+            finally
+            {
+                if (connection.State != ConnectionState.Closed)
+                {
+                    connection.Close();
+                }
+            }
+
+
+
+
+        }
+
+        private void buttonDelete_Click(object sender, EventArgs e)
+        {
+            int res = -1;
+            if ( dataGridView1.SelectedRows.Count == 0)
+            {
+                return;
+            }
+
+            DialogResult dialogResult = MessageBox.Show("Numara silinecek, onaylıyor musunuz ?", "Onay", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+            object value = dataGridView1.SelectedRows[0].Cells[0].Value;
+            int id = (int)value;
+
+            connection = new SqlConnection("Data Source=DESKTOP-Q9BA0JM;Initial Catalog=TelephoneBook;Integrated Security=True");
+            command = new SqlCommand("delete Persons where [id]=@id", connection);
+            command.Parameters.AddWithValue("@id", id);
+            try
+            {
+                connection.Open();
+                res = command.ExecuteNonQuery();
+                MessageBox.Show($"Telefon numarası sili{(res>0 ? "ndi" : "nemedi")}." );
+            }
+            catch (Exception)
+            {
+                
+
+            }
+            finally
+            {
+                if (connection.State != ConnectionState.Closed)
+                {
+                    connection.Close();
+                }
+            }
+
+            if (res>0)
+            {
+                Listing();
+            }
+            
+        }
+
+        private void textBoxSearch_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void backgroundWorker2_DoWork(object sender, DoWorkEventArgs e)
+        {
+
+        }
+
+        private void buttonUpdate_Click(object sender, EventArgs e)
+        {
+            int result = -1;
+            if (dataGridView1.SelectedRows.Count == 0)
+            {
+                return;
+            }
+
+            object value = dataGridView1.SelectedRows[0].Cells[0].Value;
+            int id = (int)value;
+            connection = new SqlConnection("Data Source=DESKTOP-Q9BA0JM;Initial Catalog=TelephoneBook;Integrated Security=True");
+            command = new SqlCommand("update Persons set name=@n, surname=@s, telephone=@t where id=@id", connection);
+            command.Parameters.AddWithValue("@id", id);
+            command.Parameters.AddWithValue("@n", textBoxName.Text);
+            command.Parameters.AddWithValue("@s", textBoxSurname.Text);
+            command.Parameters.AddWithValue("@t", textBoxNumber.Text);
+
+
+            try
+            {
+                connection.Open();
+                result = command.ExecuteNonQuery();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Bağlantıda hata oluştu.");
+
+            }
+            finally
+            {
+                if (connection.State != ConnectionState.Closed)
+                {
+                    connection.Close();
+                }
+            }
+            if (result > 0)
+            {
+                MessageBox.Show("Telefon numarası kaydedildi.");
+                Listing();
+            }
+            else
+            {
+                MessageBox.Show("Telefon numarası kaydedilemedi.");
+                
+            }
+
+           
+
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBoxName_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
